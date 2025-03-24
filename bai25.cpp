8aaf70b84bb2cb3a35e04405f19d0f5654bccb3c
dia-proximate <kitng2402@gmail.com>
2025-03-24 17:40:00 +0700

    Add new C++ programs for area calculation and addition

---
+++ bai25.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	int a , b, c;
+
+    cin >> a >> b;
+    c = a + b;
+	cout << "a + b = " << c;
+    // int int_c = stoi(c);
+    // cout << "a + b = " << int_c << endl;
+    return 0;
+}
+// What is the output of the following code?
+// a + b = 97
