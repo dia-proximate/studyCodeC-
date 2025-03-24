b92b92d87b9159230d7872914df3054b79edd227
dia-proximate <kitng2402@gmail.com>
2025-03-24 17:48:41 +0700

    Add C++ program to calculate modulus of two integers

---
+++ bai27.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	int a,b; 
+    cin >> a >> b;
+    cout << "a % b = " << a%b;
+	return 0;
+}
+// What is the output of the following code?
+// a % b = 1
